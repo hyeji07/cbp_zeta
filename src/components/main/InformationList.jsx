c38460fe08328e88577b10d05eec46f27559e950
hyeji07 <zindex_ann@naver.com>
2022-09-21 15:48:12 +0900

    main_title수정

--- src/components/main/InformationList.jsx
+++ src/components/main/InformationList.jsx
@@ -8,7 +8,7 @@ const InformationList = () => {
   const totalData = ['M&A・투자정보', '정부지원사업 참여 모집', '정책자금 / 기업지원정보', '기술거래 리스트', '컨설팅 실적', 'Q&A'];
   return (
     <div className="mainInformation">
-      <h2 className="mainInfoTitle">ZETA INFOMATION</h2>
+      <h2 className="mainInfoTitle">ZETA INFORMATION</h2>
       <div className='infoSwiperBoxList'>
         {
           totalData.map((item, index)=> {
